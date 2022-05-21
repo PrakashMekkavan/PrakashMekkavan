61cee35d73feefebb25b16d3cc71b3fd0df4c55d
Prakash <prakash@gmail.com>
2022-05-21 14:53:37 +0530

    msd

---
+++ src/org/emp/Company.java
@@ -0,0 +1,15 @@
+package org.emp;
+// import packagename.classname;
+public class Company {
+	public void CompanyID() {
+		// TODO Auto-generated method stub
+System.out.println("Hello Comp Id");
+	}
+public static void main(String[] args) {
+	Company w= new Company();
+	w.CompanyID();
+	Employee e= new Employee();
+	e.EmpId();
+	e.EmpName();
+}
+}
---
+++ src/org/emp/Employee.java
@@ -0,0 +1,34 @@
+package org.emp;
+
+public class Employee {
+
+	public void EmpId() {
+		System.out.println(" Hello Employee id");
+		// TODO Auto-generated method stub
+
+	}
+	public void EmpName() {
+		// TODO Auto-generated method stub
+System.out.println("Hello Employee Name");
+	}
+	private void EmpPhone() {
+		// TODO Auto-generated method stub
+System.out.println("Hello Employee Phonee");
+	}
+private void EmpDob() {
+	// TODO Auto-generated method stub
+System.out.println("hello employee dob");
+}
+private void EmpAdrss() {
+	// TODO Auto-generated method stub
+System.out.println("Hello $$$$$  Adress");
+}
+ public static void main(String[] args) {
+	Employee a= new Employee();
+	a.EmpName();
+	a.EmpPhone();
+	a.EmpDob();
+	a.EmpAdrss();
+	a.EmpId();
+}
+}
